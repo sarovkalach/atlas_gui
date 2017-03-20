3a5fc225c58dc1814c1c4ead2ab0890e9fe31197
kalachov_alex <kalachovalex@gmail.com>
2017-03-20 11:05:31 +0300

    Trash from main was deleted

--- integrator/src/main.cpp
+++ integrator/src/main.cpp
@@ -31,39 +31,8 @@ int main(int argc, char *argv[])
         qDebug() << "Error for database " << db.databaseName() << " :" << db.lastError().text() << endl;
     }
 
-    //QTranslator translator;
-
-    //translator.load("/home/kalach/Qt/GUI/main_ru.qm");
-    //app.installTranslator(&translator);
-
-    //TopLevelWidget widget;
-    //widget.historyWidget->showHistoryTable(1);
-    //BookmarkWidget widget;
-    //widget.showBookmarkTable(1);
-    //NoteWidget widget;
-    //widget.showNoteTable(1);
-    //MainWindow widget;
-    //widget.setApp(&app);
-    //qDebug() <<  widget.translator->isEmpty();
-    //HistoryWidget widget;
-    //widget.showHistoryTable(1);
-    //UserWidget widget;
-
-    //GlossaryWidget widget;
-    //widget.showNoteTable(2);
-     //widget.show();
-    //QPushButton hello(QPushButton::tr("Hello world!"));
-    //hello.resize(100, 30);
-    //hello.show();
-
     StartWindow widget;
     widget.setApp(&app);
-    //widget.show();
-
-    //IntegrateWidget widget;
-    //SimulatorWidget widget;
-
-    //NotepadWidget widget;
     widget.show();
 
     return app.exec();
